74b93cda61175d88522e9201b1ded56d3c24dd0b
Diego Sousa <me@diego.dev.br>
2021-05-03 16:44:18 -0300

    Basic static components

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,15 @@
+import Logo from 'assets/img/ds-logo.svg';
+
+const NavBar = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div className="container">
+                <nav className="my-2 my-md-0 mr-md-3">
+                    <img src={Logo} alt="DevSuperior" width="120" />
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
