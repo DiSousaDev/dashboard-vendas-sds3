62dde79ff3505e3b44490c68510e44ac8094e94b
Diego Sousa <me@diego.dev.br>
2021-05-03 13:57:39 -0300

    Create project

---
+++ backend/src/main/java/br/dev/diego/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package br.dev.diego.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
