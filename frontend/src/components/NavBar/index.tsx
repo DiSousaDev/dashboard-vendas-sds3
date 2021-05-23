ff2746294df97a95fb79fc6325ef7f08176a8715
Éder Diego de Sousa <me@diego.dev.br>
2021-05-23 15:09:25 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
 import Logo from 'assets/img/ds-logo.svg';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={Logo} alt="DevSuperior" width="120" />
+                    <Link to="/">
+                        <img src={Logo} alt="DevSuperior" width="120" />
+                    </Link>
                 </nav>
             </div>
         </div>
